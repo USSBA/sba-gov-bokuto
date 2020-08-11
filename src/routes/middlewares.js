0b4b0a1ea2f6358bcde734e56704a3ce4bbf9b21
Ryan Hillard <ryan.hillard@sba.gov>
2020-08-11 16:13:09 -0500

    Fix error in merge

--- src/routes/middlewares.js
+++ src/routes/middlewares.js
@@ -7,11 +7,7 @@ module.exports = {
         // console.log(req.headers.cookie.eSessionCookie)
         // console.log(req.headers.cookie.eSessionCookiePlain)
         // console.log('Cookies: ', req.cookies)
-<<<<<<< HEAD
         // console.log('Signed Cookises: ', cookieParser.signedCookies(req.cookies['eSessionCookie'], EXTERNAL_SESSION_SECRET))
-=======
-        // console.log('Signed Cookies: ', cookieParser.signedCookies(req.cookies['eSessionCookie'], EXTERNAL_SESSION_SECRET))
->>>>>>> f8bdb5905a8c512646e8382b9d1826243df8d4d3
         // console.log()
         if (!req.session.userId) {
             console.log("Session missing!")

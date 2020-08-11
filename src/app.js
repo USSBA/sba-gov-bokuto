953320e44ba05bc6eb2d6805b29e7fdba924d80d
Ryan Hillard <ryan.hillard@sba.gov>
2020-08-11 17:18:20 -0500

    Add header logging

--- src/app.js
+++ src/app.js
@@ -72,6 +72,8 @@ dynamoDb.scan(params, function(err, data) {
 // Landing page with instructions
 app.get('/', function(request, response) {
 	console.log('GET: Root route accessed')
+	console.log(request.header('Access-Control-Allow-Origin'))
+	console.log(request.header('Access-Control-Allow-Credentials'))
 	console.log('Cookies: ', request.cookies)
 	console.log('eSessionCookiePlain: ', request.cookies['eSessionCookiePlain'])
 	console.log('lSessionCookiePlain: ', request.cookies['lSessionCookiePlain'])

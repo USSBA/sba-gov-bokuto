f5bdc1fee051495ed76628848228513252a68d2c
Ryan Hillard <ryan.hillard@sba.gov>
2020-08-11 15:34:05 -0500

    Add logging to see external cookies

--- src/routes/middlewares.js
+++ src/routes/middlewares.js
@@ -1,10 +1,13 @@
 module.exports = {
     requireAuth(req, res, next) {
+        console.log(req.session.eSessionCookie)
+        console.log(req.session.eSessionCookiePlain)
         if (!req.session.userId) {
-            return res.redirect('/signin')
+            console.log("Session missing!")
+            // return res.redirect('/signin')
         } else {
             console.log("Session found!")
-            res.locals.user = req.session.userId;
+            // res.locals.user = req.session.userId;
         }
 
         next()
